8581911d89bd440637a1ca8dfdd7d0d620d2bb9d
riezenmark <riezenmark@gmail.com>
2020-05-10 19:07:24 +1000

    JavaDoc

--- job4j/Chapter_001/src/main/java/loop/PrimeNumber.java
+++ job4j/Chapter_001/src/main/java/loop/PrimeNumber.java
@@ -1,8 +1,21 @@
 package loop;
 
+/**
+ * Works with prime numbers.
+ *
+ * @author riezenmark (riezenmark@gmail.com)
+ */
 public class PrimeNumber {
+
+    /**
+     * Counts amount of prime numbers up to parameter.
+     * @param finish Number until which counting is kept.
+     * @return Amount of prime numbers.
+     */
     public int calc(int finish) {
+        
         int count = 0;
+
         for (int currentNumber = 2; currentNumber <= finish; currentNumber++) {
             int dividers = 0;
             for (int j = 1; j <= currentNumber; j++) {
---
+++ job4j/Chapter_001/src/main/java/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for tasks with loops.
+ *
+ * @author riezenmark (riezenmark@gmail.com)
+ * @version 1.0
+ * @since 0.1
+ */
+package loop;
