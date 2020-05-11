c1732b36c1a784de83f177968683d969d1ced570
riezenmark <riezenmark@gmail.com>
2020-05-11 19:50:30 +1000

    class fields

--- job4j/Chapter_002/src/main/java/oop/Cat.java
+++ job4j/Chapter_002/src/main/java/oop/Cat.java
@@ -1,17 +1,40 @@
 package oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
 
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String dish) {
+        this.food = dish;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
-
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.giveNick("Chernish");
+        black.eat("fish");
+        black.show();
     }
 }
