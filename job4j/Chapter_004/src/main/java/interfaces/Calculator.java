6a71b436d41fafc53ae31058ff894c70d49fe62c
riezenmark <riezenmark@gmail.com>
2020-05-29 18:13:31 +1000

    Functional interfaces and lambdas

---
+++ job4j/Chapter_004/src/main/java/interfaces/Calculator.java
@@ -0,0 +1,61 @@
+package interfaces;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Calculator {
+
+    public interface Operation {
+        double calculation(int left, int right);
+    }
+
+    public void calculate(int start, int finish, int value, Operation op) {
+        for (int index = start; index != finish; index++) {
+            System.out.println(
+                    op.calculation(value, index)
+            );
+        }
+    }
+
+    public void calculate(int start, int finish, int value,
+                         BiFunction<Integer, Integer, Double> op,
+                         Consumer<Double> media) {
+        for (int index = start; index != finish; index++) {
+            media.accept(op.apply(value, index));
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.calculate(0, 10, 2, new Operation() {
+            @Override
+            public double calculation(int left, int right) {
+                return left * right;
+            }
+        });
+
+        calc.calculate(
+                0, 10, 2,
+                (value, index) -> value * index
+        );
+
+        calc.calculate(
+                0, 10, 2,
+                (value, index) -> {
+                    int result = value * index;
+                    System.out.printf("Multiple %s * %s = %s %n", value, index, result);
+                    return result;
+                }
+        );
+
+        calc.calculate(
+                0, 10, 2,
+                (value, index) -> {
+                    double result = value * index;
+                    System.out.printf("Multiple %s * %s = %s %n", value, index, result);
+                    return result;
+                },
+                result -> System.out.println(result)
+        );
+    }
+}
