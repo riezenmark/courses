4409e506436347ce3d124181d4e479010913ddca
riezenmark <riezenmark@gmail.com>
2020-06-01 02:02:01 +1000

    Simple Iterator

---
+++ job4j/Chapter_005/src/main/java/collections/IteratorIntArray.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.Iterator;
+
+public class IteratorIntArray implements Iterator {
+
+    private final int[] values;
+    private int index = 0;
+
+    public IteratorIntArray(final int[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return values.length > index;
+    }
+
+    @Override
+    public Object next() {
+        return values[index++];
+    }
+}
---
+++ job4j/Chapter_005/src/test/java/collections/IteratorIntArrayTest.java
@@ -0,0 +1,66 @@
+package collections;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class IteratorIntArrayTest {
+
+    private static final class ForEachArray implements Iterable {
+
+        private final int[] values;
+
+        public ForEachArray(final int[] values) {
+            this.values = values;
+        }
+
+        @Override
+        public Iterator iterator() {
+            return new IteratorIntArray(this.values);
+        }
+    }
+
+    @Test
+    public void whenCallNextThenGetValueAndGoForward() {
+        IteratorIntArray it = new IteratorIntArray(new int[] {1, 3});
+
+        it.next();
+        var result = it.next();
+
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenHasNext() {
+        IteratorIntArray it = new IteratorIntArray(new int[] {1, 3});
+
+        it.next();
+        boolean hasNext = it.hasNext();
+
+        assertThat(hasNext, is(true));
+    }
+
+    @Test
+    public void whenNoNext() {
+        IteratorIntArray it = new IteratorIntArray(new int[] {1, 3});
+
+        it.next();
+        it.next();
+        boolean hasNext = it.hasNext();
+
+        assertThat(hasNext, is(false));
+    }
+
+    @Test
+    public void whenForEach() {
+        ForEachArray f = new ForEachArray(new int[] {1, 2, 4, 5, 10, 12});
+
+        for (var value : f) {
+            System.out.println(value);
+        }
+    }
+
+}
