03ddbf11905103b514c7dd9d2b4abc02536a2897
riezenmark <riezenmark@gmail.com>
2020-07-08 23:53:17 +1000

    "Ping-Pong"

---
+++ job4j/Chapter_010/src/main/java/pingpong/PingPong.java
@@ -0,0 +1,25 @@
+package pingpong;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class PingPong extends Application {
+    private static final String NAME = "Ping-Pong";
+
+    @Override
+    public void start(Stage stage) {
+        int limitX = 300;
+        int limitY = 300;
+        Group group = new Group();
+        Rectangle rectangle = new Rectangle(50, 100, 10, 10);
+        group.getChildren().add(rectangle);
+        new Thread(new RectangleMove(rectangle)).start();
+        stage.setScene(new Scene(group, limitX, limitY));
+        stage.setTitle(NAME);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
---
+++ job4j/Chapter_010/src/main/java/pingpong/RectangleMove.java
@@ -0,0 +1,42 @@
+package pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rectangle;
+    private boolean right = true;
+
+    public RectangleMove(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    private void setSide() {
+        if (this.rectangle.getX() == 290) {
+            this.right = false;
+        }
+        if (this.rectangle.getX() == 0) {
+            this.right = true;
+        }
+    }
+
+    private void move() {
+        setSide();
+        if (this.right) {
+            this.rectangle.setX(this.rectangle.getX() + 1);
+        } else {
+            this.rectangle.setX(this.rectangle.getX() - 1);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            move();
+            try {
+                Thread.sleep(51);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
